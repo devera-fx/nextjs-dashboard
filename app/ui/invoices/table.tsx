b6695f2fc5bb9b8868a2d78c9e232270d79cce47
Najib ul Rehman Malik <aakifnajib@gmail.com>
2024-12-02 20:32:20 +0500

    completed dashboard

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -4,6 +4,7 @@ import InvoiceStatus from '@/app/ui/invoices/status';
 import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
 import { fetchFilteredInvoices } from '@/app/lib/data';
 
+
 export default async function InvoicesTable({
   query,
   currentPage,
@@ -46,10 +47,10 @@ export default async function InvoicesTable({
                     </p>
                     <p>{formatDateToLocal(invoice.date)}</p>
                   </div>
-                  <div className="flex justify-end gap-2">
+                  <td className="flex justify-end gap-2 whitespace-nowrap px-6 py-4 text-sm">
                     <UpdateInvoice id={invoice.id} />
                     <DeleteInvoice id={invoice.id} />
-                  </div>
+                  </td>
                 </div>
               </div>
             ))}
